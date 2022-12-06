b98bddc4ba7234d1265a924fb5b86a257a161578
al.khf_ <90599670+alkhf11@users.noreply.github.com>
2022-12-06 17:20:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 pipe = pickle.load(open('pipe.pkl', 'rb'))
 df = pickle.load(open('df.pkl', 'rb'))
 
-st.title("Laptop Price Predictor")
+st.title("Laptop Price Predictor 2023")
 
 #brand laptop
 company = st.selectbox('Brand', df['Company'].unique())
